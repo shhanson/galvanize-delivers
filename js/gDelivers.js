d5d9a190590cb31031e0995a62dab9d2c7952daf
Sarah Hanson <sarahh.hanson@gmail.com>
2017-05-05 17:02:51 -0500

    Setting up JavaScript

--- js/gDelivers.js
+++ js/gDelivers.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Handler when the DOM is fully loaded
-    //$(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
+
+
+    $("#addburger").click(function(){
+        //add row to #orderstable
+        //update #totalstable
+    });
+
+    $("#addpizza").click(function(){
+        //add row to #orderstable
+        //update #totalstable
+    });
+
+    $("#addicecream").click(function(){
+        //add row to #orderstable
+        //update #totalstable
+    });
+
+    $("#addswine").click(function(){
+        //add row to #orderstable
+        //update #totalstable
+    });
+
+    $("#orderbtn").click(function(){
+        //if #orderstable empty or input fields empty, toast validation message
+        // Materialize.toast('I am a toast!', 4000) // 4000 is the duration of the toast
+    });
 
 
 });
